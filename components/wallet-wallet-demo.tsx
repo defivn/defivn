80128a5e8ed5207f4dc5b6304ac1a36b1bebf083
ZxStim <zxstim@gmail.com>
2025-06-27 17:59:47 +0700

    feat: added wallet creation

--- components/wallet-wallet-demo.tsx
+++ components/wallet-wallet-demo.tsx
@@ -3,16 +3,22 @@
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
-import { sha256, toHex } from "viem"
+import { generatePrivateKey, generateMnemonic, english, privateKeyToAccount, mnemonicToAccount } from 'viem/accounts'
 
 export default function WalletDemo() {
-  const [data, setData] = useState("")
+  const [text, setText] = useState("")
+
+  const account = text === "" ? "" : text.startsWith("0x") ? privateKeyToAccount(text as `0x${string}`) : mnemonicToAccount(text)
 
   return (
     <div className="flex flex-col gap-4 mt-4">
-      <Textarea placeholder="Nhập dữ liệu vào đây" value={data} onChange={(e) => setData(e.target.value)} />
-      <Button className="w-fit self-end hover:cursor-pointer" onClick={() => setData("")}>Xoá dữ liệu</Button>
-      <Textarea placeholder="Kết quả băm dữ liệu sẽ hiện ra ở đây" value={data ? sha256(toHex(data)) : ""} readOnly />
+      <div className="flex flex-row gap-2">
+        <Button className="w-fit self-end hover:cursor-pointer" onClick={() => setText(generatePrivateKey())}>Tạo private key</Button>
+        <Button className="w-fit self-end hover:cursor-pointer" onClick={() => setText(generateMnemonic(english))}>Tạo mnemonic</Button>
+        <Button className="w-fit self-end hover:cursor-pointer" onClick={() => setText("")}>Xoá</Button>
+      </div>
+      <Textarea placeholder="Bí mật được tạo" value={text} readOnly />
+      <Textarea placeholder="Địa chỉ ví tương ứng" value={account && typeof account === 'object' ? account.address : ""} readOnly />
     </div>
   )
 }
