d8e8cb7d3dddf8acd849b4a164785e95294fcc51
lou1s <lou1svuong.dev@gmail.com>
2025-06-28 16:54:23 +0700

    feat: integrate blockchain components into MDX components for enhanced functionality

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,10 @@
 import type { MDXComponents } from "mdx/types";
 import { mdxComponents } from "@/components/mdx";
+import { blockChainComponents } from "@/components/blockchain";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...mdxComponents,
+    ...blockChainComponents,
     ...components,
   };
 }
