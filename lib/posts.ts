a4b311746ced5c53ef0d644be006ecc36891e92f
ZxStim <zxstim@gmail.com>
2025-07-04 23:33:21 +0700

    feat: removed yield farming and moved to glossary

--- lib/posts.ts
+++ lib/posts.ts
@@ -10,28 +10,28 @@ export const posts = [
       },
       {
         id: 2,
+        title: "Tài sản số",
+        url: "/tai-lieu/tai-san-so"
+      },
+      {
+        id: 3,
         title: "Tài chính phi tập trung",
         url: "/tai-lieu/tai-chinh-phi-tap-trung"
       },
       {
-        id: 3,
+        id: 4,
         title: "Ứng dụng phi tập trung",
         url: "/tai-lieu/ung-dung-phi-tap-trung"
       },
       {
-        id: 4,
+        id: 5,
         title: "Hợp đồng thông minh",
         url: "/tai-lieu/hop-dong-thong-minh"
       },
       {
-        id: 5,
+        id: 6,
         title: "Tổ chức tự trị phi tập trung",
         url: "/tai-lieu/to-chuc-tu-tri-phi-tap-trung"
-      },
-      {
-        id: 6,
-        title: "Canh tác lãi suất",
-        url: "/tai-lieu/canh-tac-lai-suat"
       },
       {
         id: 7,
