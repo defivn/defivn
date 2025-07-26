1cb4273dfdf4fd83d8b43fdd3b8eba83b8583315
ZxStim <zxstim@gmail.com>
2025-07-26 17:04:09 +0700

    feat: added info to student guide

--- lib/posts.ts
+++ lib/posts.ts
@@ -155,8 +155,8 @@ export const posts = [
     posts: [
       {
         id: 1,
-        title: "Lựa chọn nền tảng công nghệ blockchain cho dự án tài sản số",
-        url: "/tai-lieu/lua-chon-nen-tang-cong-nghe-blockchain-cho-du-an-tai-san-so"
+        title: "Vì sao Layer 2 trên Ethereum là lựa chọn hợp lý hơn Layer 1?",
+        url: "/tai-lieu/vi-sao-layer-2-tren-ethereum-la-lua-chon-hop-ly-hon-layer-1"
       }
     ]
   }
